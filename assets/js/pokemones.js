7fa8bf4bade98b5091c98db4869868ac5e3bffba
Elian Rojas <impresosgosen@gmail.com>
2021-04-13 16:01:02 -0600

    Modal ready

--- assets/js/pokemones.js
+++ assets/js/pokemones.js
@@ -1,4 +1,3 @@
-//Llamado con javascript
 document.addEventListener('DOMContentLoaded', function(){
   const state = {
     pokemons: [],
@@ -7,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function(){
   const pokemons = document.querySelector('ul.pokemons')
 
   document.addEventListener('click', function(e){
-    if (e.target.matches('[href = "#"]')){
+    if (e.target.matches('[href="#"]')){
       e.preventDefault()
     }
 
@@ -18,32 +17,29 @@ document.addEventListener('DOMContentLoaded', function(){
     if (e.target.matches('.pokemon-detail')){
       e.preventDefault()
       return fetchPokemon(e.target.href)
-    
-    }
+    }    
 
     if (e.target.closest('.pokemon-detail')){
       e.preventDefault()
       fetchPokemon(e.target.parentNode.href)
     }
   })
+
   function fetchPokemons(){
     fetch('https://pokeapi.co/api/v2/pokemon/' + `?offset=${state.offset}`, {
       method: 'GET', 
       headers: {
         'Content-Type': 'application/json'
       }
-
-      //data es el contenido de la promesa que trae el fetch
-      // siempres debemos hacer el then data y then pokemons
-      // dentro del objeto pokemons hay un array llamado results
     }).then(function(data){ return data.json() })
       .then(function(pokemons){ addPokemons(pokemons.results) })
       .then(function(){ state.offset += 20 })
   }
-  function fetchPokemon(url){ 
+
+  function fetchPokemon(url){
     fetch(url)
-    .then(function(data){ return data.json() })
-    .then(function(pokemon){ renderModal(pokemons) })
+      .then(function(data){ return data.json() })
+      .then(function(pokemon){ renderModal(pokemon)})
   }
 
   function addPokemons(pokemons){
@@ -52,12 +48,16 @@ document.addEventListener('DOMContentLoaded', function(){
   }
 
   function renderModal(poke){
+    console.log(poke)
     const poke_modal = document.createElement('div')
     poke_modal.className = 'modal fade'
+
+    const poke_types = poke.types.map(function(p){ return `<li>${p.type.name}</li>`})
+
     const html = `<div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header border-0">
-                <h5 class="modal-title" id="pokemon-data-name"></h5>
+                <h5 class="modal-title" id="pokemon-data-name">${poke.name}</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -67,7 +67,7 @@ document.addEventListener('DOMContentLoaded', function(){
                     <tr>
                         <th>Tipos</th>
                         <td>
-                            <ol id="pokemon-types"></ol>
+                            <ol id="pokemon-types">${poke_types.join('')}</ol>
                         </td>
                     </tr>
                     <tr>
@@ -86,36 +86,39 @@ document.addEventListener('DOMContentLoaded', function(){
             </div>
         </div>
 </div>`
+
     poke_modal.innerHTML = html
     document.body.appendChild(poke_modal)
 
-  $('.modal').modal('show')
-  $('.modal').on('hidden.bs.modal', function(e){
-    $(this).remove()
-  })
+    $('.modal').modal('show')
+    $('.modal').on('hidden.bs.modal', function(){
+      $(this).remove()
+    })
   }
-
-
 
   function renderPokemons(){
    state.pokemons.slice(state.offset).map(function(poke){
      const li = document.createElement('li')
      li.className = 'list-group-item'
+
      const link = document.createElement('a')
      link.className = 'pokemon-detail d-flex justify-content-between align-items-center'
      link.href = poke.url
+
      const text = document.createTextNode(poke.name)
+
      const span = document.createElement('span')
      span.className = 'badge badge-primary badge-pill'
      span.innerText = '›'
 
-     //innerText = document.createTextNode
      
      link.appendChild(text)
      link.appendChild(span)
      li.appendChild(link)
+
      pokemons.appendChild(li)
    })
   }
+
   fetchPokemons()
 })
